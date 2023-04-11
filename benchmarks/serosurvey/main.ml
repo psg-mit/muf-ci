c04173fa74f28fcf25d8451c7bc2d37eabc78629
Ellie Cheng <ellieyhc@csail.mit.edu>
2023-04-10 20:56:04 -0400

    Remove intermediate files

--- benchmarks/serosurvey/main.ml
+++
@@ -1,9 +0,0 @@
- open Muf 
-
-(* simulation (discrete) function *)
-let main =
-  let mem = ref (Muflib.init Serosurvey_default.main) in
-  (fun x -> let _, s = Muflib.step !mem x in mem := s);;
-(* (discrete) simulation loop *)
-while true do main () done;
-exit(1);;
