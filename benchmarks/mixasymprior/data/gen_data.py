0ae56028299a2e542e9d7aaaac2c2b74c8504acd
Ellie Cheng <ellieyhc@csail.mit.edu>
2023-06-15 22:41:18 -0700

    Mix asym prior data fix

--- benchmarks/mixasymprior/data/gen_data.py
+++ benchmarks/mixasymprior/data/gen_data.py
@@ -3,8 +3,8 @@ import numpy as np
 n = 100
 
 theta = 0.3
-mu1 = np.random.normal(2.75, 0.5)
-mu2 = np.random.normal(-2.75, 0.5)
+mu1 = np.random.normal(2.75, np.sqrt(0.5))
+mu2 = np.random.normal(-2.75, np.sqrt(0.5))
 var1 = np.random.gamma(1, 1)
 var2 = np.random.gamma(1, 1)
 
@@ -16,8 +16,8 @@ with open('data.csv', 'w') as f:
 
 for i in range(n):
     if np.random.random_sample() < theta:
-        y = np.random.normal(mu1, 1/var1)
+        y = np.random.normal(mu1, np.sqrt(1/var1))
     else:
-        y = np.random.normal(mu2, 1/var2)
+        y = np.random.normal(mu2, np.sqrt(1/var2))
     with open('data.csv', 'a') as f:
         f.write('{}\n'.format(y))
