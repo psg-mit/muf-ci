4a4c1b913465c3032ab05846fe0b92cf625ddb19
Ellie Cheng <ellieyhc@csail.mit.edu>
2024-03-28 02:02:08 -0400

    example2

---
+++ benchmarks/example2/programs/data/gen_data.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+n = 100
+h = 2
+f = 1.001
+
+q = 2
+r = 1
+
+print(f'q: {q}\nr: {r}')
+
+with open('data.csv', 'w') as out:
+  out.write('true_x, obs\n')
+
+# j = np.random.randint(0, n)
+j = 200
+
+x0 = 0
+
+prev_x = x0
+for i in range(n):
+  x = np.random.normal(prev_x, np.sqrt(q))
+
+  env = i == j
+  if env:
+    other = np.random.uniform(900, 1000)
+    z = np.random.normal(x, np.sqrt(r + other))
+  else:
+    z = np.random.normal(x, np.sqrt(r))
+
+  prev_x = x
+
+  with open('data.csv', 'a') as out:
+    out.write(str(x) + ', ' + str(z) + '\n')
