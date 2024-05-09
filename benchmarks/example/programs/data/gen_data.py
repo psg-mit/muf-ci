8da1a97515ea77403cfcac55845621637a6f269e
Ellie Cheng <ellieyhc@csail.mit.edu>
2024-05-09 11:58:55 -0700

    clean up benchmark folder

--- benchmarks/example/programs/data/gen_data.py
+++ benchmarks/example/programs/data/gen_data.py
@@ -2,23 +2,26 @@ import numpy as np
 
 n = 100
 h = 2
-f = 1.001
+f = 5
 
 q = 2
-r = 1
+r = 5
 
 print(f'q: {q}\nr: {r}')
 
 with open('data.csv', 'w') as out:
   out.write('true_x, obs\n')
+with open('true_x.csv', 'w') as out:
+  out.write('true_x\n')
 
-j = np.random.randint(0, n)
+# j = np.random.randint(0, n)
+j = 200
 
 x0 = 0
 
 prev_x = x0
 for i in range(n):
-  x = np.random.normal(prev_x, np.sqrt(q))
+  x = np.random.normal(prev_x+f, np.sqrt(q))
 
   env = i == j
   if env:
@@ -31,3 +34,5 @@ for i in range(n):
 
   with open('data.csv', 'a') as out:
     out.write(str(x) + ', ' + str(z) + '\n')
+  with open('true_x.csv', 'a') as out:
+    out.write(str(x) + ',\n')
